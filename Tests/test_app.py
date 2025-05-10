84cf2c75123803f2acaf2d4b9f06b44f9e32c221
Claire Holmes <holmes.claire13@gmail.com>
2025-05-10 10:44:26 -0500

    no idea

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -4,13 +4,16 @@
 import unittest
 import app as app
 
-
 class TestApp(unittest.TestCase):
     '''class for tests for app.py'''
 
+    def setUp(self):
+        '''set up for testing'''
+        self.app = app.app.test_client()
+
     def test_homepage(self):
         '''test homepage returns the correct message'''
-        response = app.get('/')
+        response = self.app.get('/')
         self.assertEqual("Hello, this is the homepage for the leisure time data. " \
             "To see the most common activitiy that people spend their time on for a certain age" \
             " add a '/' to this URL followed by the age you would like to look at " \
@@ -18,10 +21,10 @@ class TestApp(unittest.TestCase):
         
     def test_get_top_activity(self):
         '''test get_top_activity returns the correct top activity for the given age'''
-        response = app.get('/23')
+        response = self.app.get('/23')
         self.assertEqual("Financial management", response.data)
 
     def test_get_top_activity_invalid_age(self):
         '''test get_top_activity returns the correct error message for invalid age'''
-        response = app.get('/100')
+        response = self.app.get('/100')
         self.assertEqual("Invalid age, choose a valid age based on the small dataset: 18, 23, 40, 56, 57, 71, 80", response.data)
