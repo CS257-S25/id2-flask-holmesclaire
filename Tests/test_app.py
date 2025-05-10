a2b193af601cfb48b1fd640f84ff5e6bfa09ec7a
Claire Holmes <holmes.claire13@gmail.com>
2025-05-10 10:46:45 -0500

    forgot b

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -14,17 +14,17 @@ class TestApp(unittest.TestCase):
     def test_homepage(self):
         '''test homepage returns the correct message'''
         response = self.app.get('/')
-        self.assertEqual("Hello, this is the homepage for the leisure time data. " \
-            "To see the most common activitiy that people spend their time on for a certain age" \
-            " add a '/' to this URL followed by the age you would like to look at " \
-            "(age options: 18, 23, 40, 56, 57, 71, 80).", response.data)
+        self.assertEqual(b"Hello, this is the homepage for the leisure time data. " \
+            b"To see the most common activitiy that people spend their time on for a certain age" \
+            b" add a '/' to this URL followed by the age you would like to look at " \
+            b"(age options: 18, 23, 40, 56, 57, 71, 80).", response.data)
         
     def test_get_top_activity(self):
         '''test get_top_activity returns the correct top activity for the given age'''
         response = self.app.get('/23')
-        self.assertEqual("Financial management", response.data)
+        self.assertEqual(b"Financial management", response.data)
 
     def test_get_top_activity_invalid_age(self):
         '''test get_top_activity returns the correct error message for invalid age'''
         response = self.app.get('/100')
-        self.assertEqual("Invalid age, choose a valid age based on the small dataset: 18, 23, 40, 56, 57, 71, 80", response.data)
+        self.assertEqual(b"Invalid age, choose a valid age based on the small dataset: 18, 23, 40, 56, 57, 71, 80", response.data)
