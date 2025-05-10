3202256d27bf1b3ee3d66d3599996832390d99be
Claire Holmes <holmes.claire13@gmail.com>
2025-05-10 10:55:25 -0500

    fix error tests

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -27,10 +27,13 @@ class TestApp(unittest.TestCase):
     def test_get_top_activity_invalid_age(self):
         '''test get_top_activity returns the correct error message for invalid age'''
         response = self.app.get('/100')
-        self.assertEqual(b"Invalid age, choose a valid age based on the small dataset: 18, 23, 40, 56, 57, 71, 80", response.data)
+        self.assertEqual(b"500 Internal Server Error: The server encountered an internal error and " \
+            b"was unable to complete your request. Either the server is overloaded or there is an " \
+            b"error in the application. Invalid age, choose a valid age based on the small dataset: " \
+            b"18, 23, 40, 56, 57, 71, 80", response.data)
 
-    def test_404_error(self, route):
+    def test_404_error(self):
         '''test 404 error returns the correct error message'''
-        response = self.app.get(route)
+        response = self.app.get('/invalid')
         self.assertEqual(response.status_code, 404)
         self.assertIn(b"404 Not Found: The requested URL was not found on the server. Wrong format, do this instead: ", response.data)
